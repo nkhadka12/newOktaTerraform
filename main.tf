f445d4d10eabb32353636410a0dbf05c28a1798a
Nischal Khadka <82045084+nkhadka12@users.noreply.github.com>
2023-09-19 12:56:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "okta"{
 org_name = "dev-10812830"
     base_url = "okta.com"
     client_id = "0oabe40etgZ29AVnM5d7"
-    scopes = ["okta.groups.manage"]
+    scopes = ["okta.groups.manage","okta.users.manage"]
   
 
     private_key = "${path.module}/rsa.pem"
