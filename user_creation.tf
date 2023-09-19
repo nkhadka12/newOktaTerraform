42d25f5cafa03b9783f9fd777a38115697cb9580
Nischal Khadka <82045084+nkhadka12@users.noreply.github.com>
2023-09-19 13:05:46 -0500

    Update user_creation.tf

--- user_creation.tf
+++ user_creation.tf
@@ -1,6 +1,6 @@
 resource "okta_user" "test2" {
   first_name           = "Johnny"
-  last_name            = "Smith"
+  last_name            = "Bang"
   login                = "example@example.com"
   email                = "example@example.com"
   password_inline_hook = "default"
