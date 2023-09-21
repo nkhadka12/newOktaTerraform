ea89a9cc8c7fd4171c34fe8967ca64a5a1cfd89f
Nischal Khadka <82045084+nkhadka12@users.noreply.github.com>
2023-09-21 11:28:16 -0500

    Update create_Auth_server.tf

--- create_Auth_server.tf
+++ create_Auth_server.tf
@@ -2,6 +2,6 @@ resource "okta_auth_server" "secondAuth" {
   audiences   = ["api://example"]
   description = "My Example Auth Server"
   name        = "Second_Auth_Server"
-  issuer_mode = "CUSTOM_URL"
+  issuer_mode = "Dynamic"
   status      = "ACTIVE"
 }
