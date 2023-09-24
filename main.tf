3da32683d2909c16df190868eeb96ce26160bdc3
Nischal Khadka <82045084+nkhadka12@users.noreply.github.com>
2023-09-24 15:53:40 -0500

    Hello

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
   }
 }
 provider "okta"{
-    scopes = ["okta.groups.manage","okta.users.manage","okta.authorizationServers.manage"]
+    scopes = ["okta.groups.manage","okta.users.manage","okta.authorizationServers.manage","okta.apps.manage"]
     private_key = "${path.module}/rsa.pem"
 
 }
