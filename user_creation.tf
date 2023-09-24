4ce4c12b6cb8ae43e35feb6903bcb1daf1fc2cd2
Nischal Khadka <82045084+nkhadka12@users.noreply.github.com>
2023-09-24 13:04:25 -0500

    Hee

--- user_creation.tf
+++ user_creation.tf
@@ -6,3 +6,10 @@ resource "okta_user" "test12" {
   password_inline_hook = "default"
 }
 
+resource "okta_user" "test123" {
+  first_name           = "Pohnny"
+  last_name            = "Cang"
+  login                = "example132@example.com"
+  email                = "example@example.com"
+  password_inline_hook = "default"
+}
